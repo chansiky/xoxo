0770f06612e90eed6fa118a4a42aa0c047089e7d
Diego Hernandorena <dhernandorena@gmail.com>
2018-05-16 13:57:22 -0500

    first commit

--- game/index.js
+++ game/index.js
@@ -1,6 +1,48 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+
+
+export const move = (position, player) => {
+    return {
+      type: 'move',
+      position,
+      player
+    }
 }
+
+
+
+// const initialState = {
+//   board: newBoard,
+//   player: 'X'
+// }
+
+function board(gameBoard=Map(), action) {
+  
+  return gameBoard
+}
+
+function turn() {
+  // this is to handle the player key
+}
+
+export default function reducer(game = {}, action) {
+  switch(action.type) {
+    case "move":
+      if (game.player === 'X') {
+        let nextPlayer = 'O'
+      } else {
+        let nextPlayer = 'X'
+      }
+      return {
+        board: game.board.setIn(action.position, game.player),
+        player: nextPlayer
+      }
+    default: 
+      return {
+        board: board(game.board)
+      }
+  }
+}
+
+
